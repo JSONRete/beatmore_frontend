6c9130f634fc44787f47b3d6297fd18495e6aae6
JSONRete <jsonrete@gmail.com>
2021-11-04 04:25:12 -0400

    uninstalled bootstrap'× InvalidCharacterError: Failed to execute 'createElement'npm uninstall react-bootstrap bootstrap@5.1.3'

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,14 +4,18 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home';
 import BeatsContainer from './BeatsContainer'
 import BeatsForm from './BeatsForm';
+import Navbar from './Navbar';
 
 const Router = () => {
     return (
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/beats' component={BeatsContainer} />
-            <Route exact path='/new' component={BeatsForm} />
-        </Switch>
+        <>
+            <Navbar />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/beats' component={BeatsContainer} />
+                <Route exact path='/new' component={BeatsForm} />
+            </Switch>
+        </>
     );
 };
 
