0cb92b40c696fd4a02419a46750ec3ef99094a06
JSONRete <jsonrete@gmail.com>
2021-11-07 16:31:17 -0500

    worked on Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import background from '../images/test.png'
+import "../css/home.css"
 
 const Home = () => {
     return (
-        
-        <div style={{ backgroundImage: `url(${background})` }}>
-        test
+        <div>
+       
+        <img
+        className="home"
+        src={background}
+        alt="home"
+      />
     </div>
     );
 };
 
 export default Home;
+
+// <img className="home" src={background} style={{ backgroundImage: `url(${background})` }}/>
+        
 
