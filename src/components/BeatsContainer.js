7a4e2df3a45807ad4c050160aba2efc3c8a7fd1d
JSONRete <jsonrete@gmail.com>
2021-11-08 11:44:38 -0500

    BeatsContainer.js updated

--- src/components/BeatsContainer.js
+++ src/components/BeatsContainer.js
@@ -16,10 +16,7 @@ class BeatsContainer extends Component {
     render() {
         return (
             <div>
-
-                Beats Container
                 <BeatsList />
-                
             </div>
         )
     }
