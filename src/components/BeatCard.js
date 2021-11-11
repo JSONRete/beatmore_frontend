34dfa060705ae3be6f5ac9d85be12541793ce5f3
JSONRete <jsonrete@gmail.com>
2021-11-10 19:34:00 -0500

    BeatCard.js updated

--- src/components/BeatCard.js
+++ src/components/BeatCard.js
@@ -10,7 +10,7 @@ const BeatCard = (props) => {
 
     return (
         <div className='beat-card' id={`beat-${id}`}>
-             <h4>Producer: {producer}</h4>  
+            <h4>Producer: {producer}</h4>  
             <div>
             <div style={{ width: "15rem", height: "10rem" }}>
                 <ReactPlayer
