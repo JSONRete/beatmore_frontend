141ee2132d9c0333ca8f532390d54961177e1c22
JSONRete <jsonrete@gmail.com>
2021-11-11 21:48:25 -0500

    completed FilterField.js

--- src/components/BeatsList.js
+++ src/components/BeatsList.js
@@ -8,24 +8,32 @@ class BeatsList extends React.Component{
 
     state = {
         search: ""
+    } 
+
+    inputChange = (event) => {
+        this.setState({search: event.target.value})
     }
 
+    filterProducer = () => {
+        return this.props.beats.filter( (producer) => {
+            return producer.producer.toLowerCase().includes(this.state.search.toLowerCase())
+        })
+    }
 
-   filterBeats = () => {
-    return this.props.beats
-   }
-
+    // componentDidMount() {
+    //     console.log("TESTING DID MOUNT, FROM_BEATLIST")
+    // }
 
 
     makeBeats(){
-        return this.filterBeats().map(beat => <BeatCard key={beat.id} beat={beat} />)
+        return this.filterProducer().map(beat => <BeatCard key={beat.id} beat={beat} />)
 }
     render() {
 
         
         return (
             <div className="beats">
-                <FilterField />
+                <FilterField handleChange={this.inputChange}/>
                 {this.props.loading ? <h1>LOADING....</h1> : this.makeBeats()}
             </div>
     );
--- src/components/FilterField.js
+++ src/components/FilterField.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "../css/filterfield.css"
 
-const FilterField = () => {
+const FilterField = (r) => {
     return (
         <div id="filter-field">
             <label htmlFor="filter">Filter by Producer</label>
-            <input type="text" placeholder="enter producer name"/>
+            <input type="text" placeholder="enter producer name" onChange={r.handleChange}/>
         </div>
     );
 };
