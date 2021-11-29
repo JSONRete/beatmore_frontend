6d85de49b0d3c332b3ef8d42683ced1e17c21102
JSONRete <jsonrete@gmail.com>
2021-11-29 00:03:00 -0500

    added ProducersList

---
+++ src/components/producers/ProducersList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProducersList extends Component {
+    render() {
+        return (
+            <div>
+                Testing
+            </div>
+        );
+    }
+}
+
+export default ProducersList;
