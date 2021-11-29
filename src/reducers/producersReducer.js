fdbc0e7371805e03ddb1bea269c9114f5d605dbe
JSONRete <jsonrete@gmail.com>
2021-11-29 00:03:43 -0500

    added producersReducer.js

---
+++ src/reducers/producersReducer.js
@@ -0,0 +1,9 @@
+export const producersReducer = (state = [], action) => {
+    switch(action.type){
+        case 'FETCH_PRODUCER':
+            return action.payload
+        default:
+            return state
+    }
+
+  } 
