83a5ecfcf885228f305eb2dedceedcfb45d582f5
JSONRete <jsonrete@gmail.com>
2021-12-08 23:21:58 -0500

    updated producersReducer.js

--- src/reducers/producersReducer.js
+++ src/reducers/producersReducer.js
@@ -1,6 +1,8 @@
-const producersReducer = (state = [], action) => {
+const producersReducer = (state = {artists: [], loading: false}, action) => {
     switch(action.type){
-        case 'FETCH_PRODUCER':
+        case  "LOADING":
+            return { ...state, loading: true };
+        case 'FETCH_PRODUCERS':
             return action.payload
         default:
             return state
