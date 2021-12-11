638f988dcc633280d6d3a79a0f4a3d0bbb033d4d
JSONRete <jsonrete@gmail.com>
2021-12-10 22:55:18 -0500

    added `?name=${name}` interpolation

--- src/actions/producerActions.js
+++ src/actions/producerActions.js
@@ -2,13 +2,17 @@ const url = 'http://localhost:3000/producer_pull'
 
 export const loadProducers = (artists) => ({type: 'FETCH_PRODUCERS', payload: artists})
 
-export const fetchProducers = () => {
+export const fetchProducers = (name) => {
+
+    // placeholder arg
+
     return (dispatch) => {
         dispatch({type: "LOADING"})
-        fetch(url)
+        fetch(url + `?name=${name}`)
         .then(response => response.json())
         .then(data => {
-            dispatch(loadProducers(data))
+            dispatch(loadProducers(data.artists))
+            console.log("see me", data)
 
     })
     }
