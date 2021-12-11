937de6f74c78192d9db231e0d6079468ac540d52
JSONRete <jsonrete@gmail.com>
2021-12-10 23:35:09 -0500

    working on ProducersList.js

--- src/components/producers/ProducersList.js
+++ src/components/producers/ProducersList.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ProducerCard from './ProducerCard'
 
 class ProducersList extends Component {
 
 
-
-componentDidMount() {
-    fetch('http://localhost:3000/producer_pull')
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
 
 
 
     render() {
         return (
             <div>
-                <h1> Testing h1</h1>
-                <h2> Testing h2</h2>
-                <h3> Testing h3</h3>
-                <h4> Testing h4</h4>
-                <h5> Testing h5</h5>
-                <h6> Testing h6</h6>
+                <ProducerCard />
             </div>
         );
     }
@@ -37,3 +27,5 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(ProducersList);
+
+
