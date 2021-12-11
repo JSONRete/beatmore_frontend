92e7c0876ecc0185f6682b09468ed37baa419e55
JSONRete <jsonrete@gmail.com>
2021-12-10 22:50:08 -0500

    added 'component={ProducersContainer}' to /producers path=

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,15 +3,15 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from './Home';
 import BeatsContainer from '../components/beats/BeatsContainer'
-import ProducersList from './producers/ProducersList';
 import BeatsForm from '../components/beats/BeatsForm';
+import ProducersContainer from './producers/ProducersContainer';
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/beats' component={BeatsContainer} />
-            <Route exact path='/producers' component={ProducersList} />
+            <Route exact path='/producers' component={ProducersContainer} />
             <Route exact path='/new' component={BeatsForm} />
             <Route path ="*" render={() => <h1 id="error">NO SUCH PAGE</h1>} />
         </Switch>
