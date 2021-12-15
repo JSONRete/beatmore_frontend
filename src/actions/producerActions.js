7c51d272e367fc5a7803f0afca600c75fed34af1
JSONRete <jsonrete@gmail.com>
2021-12-14 23:37:19 -0500

    added placeholder 'search' arg

--- src/actions/producerActions.js
+++ src/actions/producerActions.js
@@ -3,17 +3,17 @@ const url = 'http://localhost:3000/producer_pull'
 export const loadProducers = (artists) => ({type: 'FETCH_PRODUCERS', payload: artists})
 
 export const fetchProducers = (name) => {
-
-    // placeholder arg
-
     return (dispatch) => {
         dispatch({type: "LOADING"})
-        fetch(url + `?name=${name}`)
+        fetch(url + `?search=${name}`)
         .then(response => response.json())
         .then(data => {
             dispatch(loadProducers(data.artists))
-            console.log("see me", data)
-
+            console.log("see me from producerAction", data)
+            console.log("see me from producerAction", name)
     })
     }
 }
+
+
+  // placeholder arg
