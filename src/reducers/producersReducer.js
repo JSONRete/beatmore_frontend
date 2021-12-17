e8a411167b9f4da865da724d0734c03ad73101f0
JSONRete <jsonrete@gmail.com>
2021-12-16 22:40:03 -0500

    added SEARCH_PRODUCERS obj to producersReducer

--- src/reducers/producersReducer.js
+++ src/reducers/producersReducer.js
@@ -2,6 +2,8 @@ const producersReducer = (state = {artists: [], loading: false}, action) => {
     switch(action.type){
         case  "LOADING":
             return { ...state, loading: true };
+        case 'SEARCH_PRODUCERS':
+            return {...state, artists: [...state.artists, action.payload], loading: false}
         case 'FETCH_PRODUCERS':
             return {...state, artists: action.payload, loading: false}
         default:
@@ -9,3 +11,5 @@ const producersReducer = (state = {artists: [], loading: false}, action) => {
     }
 }
 export default producersReducer
+
+// the reducer is what handles the state of my application
