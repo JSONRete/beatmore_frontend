a33ee68e562146cf18ec68a78813b42efa4c8cec
JSONRete <jsonrete@gmail.com>
2021-12-16 22:37:56 -0500

    created makeProducers()  over

--- src/components/producers/ProducersList.js
+++ src/components/producers/ProducersList.js
@@ -4,21 +4,22 @@ import ProducerCard from './ProducerCard'
 
 class ProducersList extends Component {
 
-
-
-
-
+makeProducers = () => {
+    console.log("makeProducers()", this.props.artists)
+    return this.props.artists.map(producer => <ProducerCard key={producer.id} producer={producer} />)
+    
+}
     render() {
         return (
-            <div>
-                <ProducerCard />
-            </div>
+        <div>
+            {this.makeProducers()}
+        </div>
         );
     }
 }
 
 const mapStateToProps = state => {
-    console.log("test", state)
+    console.log("from ProducersList.js", state)
     return {
         artists: state.producersReducer.artists,
         // loading: state.loading
@@ -27,5 +28,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(ProducersList);
-
-
