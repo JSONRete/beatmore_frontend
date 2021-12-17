ec76c0aa5fbdba1399ff26b8b207fff9318fd837
JSONRete <jsonrete@gmail.com>
2021-12-17 02:12:38 -0500

    took out testing method and adjusted dispatch obj

--- src/actions/producerActions.js
+++ src/actions/producerActions.js
@@ -1,7 +1,7 @@
 const url = 'http://localhost:3000'
 
 // export const getProducers = (artists) => ({type: 'SEARCH_PRODUCERS', payload: artists})
-export const loadProducers = (artists) => ({type: 'FETCH_PRODUCERS', payload: artists})
+// export const loadProducers = (artists) => ({type: 'FETCH_PRODUCERS', payload: artists})
 
 export const searchProducers = (name) => {
     return (dispatch) => {
@@ -10,21 +10,21 @@ export const searchProducers = (name) => {
         .then(response => response.json())
         .then(data => {
             dispatch({type: 'SEARCH_PRODUCERS', payload: data})
-            console.log("see me from producerAction, data", data)
-            console.log("see me from producerAction, name ", name)
+            // console.log("see me from producerAction, data", data)
+            // console.log("see me from producerAction, name ", name)
     })
     }
 }
 
-export const fetchProducers = () => {
-    return (dispatch) => {
-        dispatch({type: "LOADING"})
-        fetch(url + `/producers`)
-        .then(response => response.json())
-        .then(data => {
-            // dispatch(loadProducers(data.artists))
-            console.log("see me from producerAction, data", data)
+// export const fetchProducers = () => {
+//     return (dispatch) => {
+//         dispatch({type: "LOADING"})
+//         fetch(url + `/producers`)
+//         .then(response => response.json())
+//         .then(data => {
+//             dispatch(loadProducers(data.artists))
+//             // console.log("see me from producerAction, data", data)
     
-    })
-    }
-}
+//     })
+//     }
+// }
