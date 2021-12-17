0d7e6e318d5c86b974d1b4acdf31596fc2474e45
JSONRete <jsonrete@gmail.com>
2021-12-16 19:16:12 -0500

    updated/changed file name TempFrom to ProducerForm

---
+++ src/components/producers/ProducersForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { searchProducers } from '../../actions/producerActions';
+import "../../css/beats/beatform.css"
+
+class ProducersForm extends Component {
+
+    state = {
+        name: ''
+    }
+
+    handleOnChange = event => {
+        const { name, value } = event.target
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.searchProducers(this.state.name)
+        this.setState({
+            name: '',
+        
+        })
+        // this.props.history.push("/beats");
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <label className='artist-card'>Producer Lookup</label>
+                <input type='text' value={this.state.name} onChange={this.handleOnChange} name='name' placeholder='enter producer name' />
+                <input type='submit' className='submitButton' value='Search Producer' />
+                </form>
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        searchProducers: (name) => dispatch(searchProducers(name))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(ProducersForm);
+
