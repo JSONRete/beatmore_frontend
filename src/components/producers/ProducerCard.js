83376567d038f0cdb68d12dde0e201a4dcd9cfe5
JSONRete <jsonrete@gmail.com>
2021-12-16 22:25:49 -0500

    updated names on producer card

--- src/components/producers/ProducerCard.js
+++ src/components/producers/ProducerCard.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
-import { withRouter } from "react-router-dom";
+import { withRouter, } from "react-router-dom";
 import { connect } from "react-redux"
-import { Link } from "react-router-dom";
+import "../../css/producers/producercard.css"
 
 class ProducerCard extends Component {
     render() {
-        console.log("this.props.producer", this.props.producer)
-        const {id ,name, gender, beginArea, area, lifeSpan} = this.props.producer
-       
+         console.log("tttt", this)
+        const {id ,name, legal_name, begin_area, birthday} = this.props.producer
+    
         return (
             <div className='producer-card' id={`producer-${id}`}>
-            <h4><Link to="/producers">Producer: {name}</Link></h4>  
-                <h4>Producer: {gender}</h4>
-                <h4>Start: {beginArea}</h4>
-                <h4>Current: {area}</h4>
-                <h4>Life Span: {lifeSpan}</h4>
+                <h4>Producer Name: {name}</h4>
+                <h4>Legal Name: {legal_name}</h4>
+                <h4>Origin: {begin_area}</h4>
+                <h4>Birthday: {birthday}</h4>
             </div>
-
         );
     }
 }
-const mapStateToProps = (state) => {
-    return {
-        producer: state.producersReducer.artists
-    }
+// const mapStateToProps = (state) => {
+//     return {
+//         producer: state.producersReducer.artists
+//     }
     
-}
-export default withRouter(connect(mapStateToProps) (ProducerCard));
-
+// }
+export default withRouter(connect(null, ) (ProducerCard));
