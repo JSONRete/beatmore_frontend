02ee11afe17c826be0aacac04835b010663c0db1
JSONRete <jsonrete@gmail.com>
2021-12-17 22:30:45 -0500

    updated ADDED_BEAT obj

--- src/reducers/beatsReducer.js
+++ src/reducers/beatsReducer.js
@@ -7,8 +7,8 @@ const beatsReducer = (state = {beats: [], loading: false}, action) => {
           // return {...state, loading: false, beats: action.payload}
           return {...state, beats: action.payload, loading: false}
       case 'ADDED_BEAT':
-          return {...state, beats: [...state.beats, action.payload]}
-          // return {beats: [...state.beats, action.payload], ...state}
+          // return {...state, beats: [...state.beats, action.payload]}
+          return {beats: [...state.beats, action.payload], ...state}
       case "DELETE_BEAT":
         return {...state, beats: state.beats.filter(beat => beat.id !== action.payload.id)}
     //   const filteredBeat = state.beats.filter(
