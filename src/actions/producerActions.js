d541b53941ed2525fdb8ea73171a0634b1796ba2
JSONRete <jsonrete@gmail.com>
2021-12-18 23:31:20 -0500

    working out fetchProducers

--- src/actions/producerActions.js
+++ src/actions/producerActions.js
@@ -1,7 +1,7 @@
 const url = 'http://localhost:3000'
 
 // export const getProducers = (artists) => ({type: 'SEARCH_PRODUCERS', payload: artists})
-// export const loadProducers = (artists) => ({type: 'FETCH_PRODUCERS', payload: artists})
+export const loadProducers = (artists) => ({type: 'FETCH_PRODUCERS', payload: artists})
 
 export const searchProducers = (name) => {
     return (dispatch) => {
@@ -16,15 +16,15 @@ export const searchProducers = (name) => {
     }
 }
 
-// export const fetchProducers = () => {
-//     return (dispatch) => {
-//         dispatch({type: "LOADING"})
-//         fetch(url + `/producers`)
-//         .then(response => response.json())
-//         .then(data => {
-//             dispatch(loadProducers(data.artists))
-//             // console.log("see me from producerAction, data", data)
+export const fetchProducers = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING"})
+        fetch(url + `/producers`)
+        .then(response => response.json())
+        .then(data => {
+            dispatch(loadProducers(data.artists))
+            // console.log("see me from producerAction, data", data)
     
-//     })
-//     }
-// }
+    })
+    }
+}
