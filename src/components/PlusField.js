a13b2d7156241ef4666673ae9cfe29c0494d92b3
JSONRete <jsonrete@gmail.com>
2021-12-23 23:43:31 -0500

    working on PlusField.js component

--- src/components/PlusField.js
+++ src/components/PlusField.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import "../css/beats/filterfield.css"
 
-const PlusField = (count) => {
-    console.log(count)
+const PlusField = () => {
     return (
         <div id="filter-field">
             <label htmlFor="filter">Add to Plus</label>
-            <input type="number" placeholder="enter starting count" onChange={count.handleChange}/>
+            <input type="number" placeholder="enter starting count" onChange={.handleChange}/>
         </div>
     );
 };
