db8b04900ff78a6471f62e0b2833b92f39e5a828
JSONRete <jsonrete@gmail.com>
2022-01-27 22:29:02 -0600

    cleaned/tightened up code

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter as Router } from 'react-router-dom'
 import rootReducer from "./reducers/rootReducer";
 import reportWebVitals from "./reportWebVitals";
-
 import "./css/index.css"
-
 import App from './components/App'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
