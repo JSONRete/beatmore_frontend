7e842a3a6ed9dc132ce9b7602159ea6cd3f303ce
JSONRete <jsonrete@gmail.com>
2022-02-07 23:58:51 -0500

    cleaned up beatsReducer.js

--- src/reducers/beatsReducer.js
+++ src/reducers/beatsReducer.js
@@ -3,18 +3,13 @@ const beatsReducer = (state = {beats: [], loading: false}, action) => {
       case "LOADING":
         return { ...state, loading: true };
       case 'FETCH_BEATS':
-        // console.log("LOG: E")
-          // return {...state, loading: false, beats: action.payload}
           return {...state, beats: action.payload, loading: false}
       case 'ADDED_BEAT':
-          // return {...state, beats: [...state.beats, action.payload]}
+
           return {beats: [...state.beats, action.payload], ...state}
       case "DELETE_BEAT":
         return {...state, beats: state.beats.filter(beat => beat.id !== action.payload.id)}
-    //   const filteredBeat = state.beats.filter(
-    //   (beat) => beat.id !== action.payload.id
-    // );
-    // return { ...state, beat: filteredBeat};
+
       default:
           return state
   }
